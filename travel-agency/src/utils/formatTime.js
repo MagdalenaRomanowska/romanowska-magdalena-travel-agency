4d04d21e98234796a4818009d3e2405bdd320f63
Magdalena Romanowska <magdalena.romanowska.poczta@gmail.com>
2020-09-08 11:55:26 +0200

    Submodule 20.3

--- travel-agency/src/utils/formatTime.js
+++ travel-agency/src/utils/formatTime.js
@@ -1,1 +1,18 @@
-export const formatTime = () => 'formatted time';
+export const formatTime = (parameter) => parameter==undefined 
+|| isNaN(parameter) 
+|| parameter < 0 ? null : ((('00' + (Math.floor(parameter/3600))).substr(-2,2))
++ ':' + (('00' + (Math.floor((parameter/60)%60))).substr(-2,2)) 
++ ':' + (('00' + (Math.floor(parameter%60))).substr(-2,2)));
+
+//isNaN(par1) - stąd: https://mkyong.com/javascript/check-if-variable-is-a-number-in-javascript/
+// Math.floor(parameter%60) - seconds; parameter%60 - reszta z dzielenia przez 60.
+
+// zero padding: https://stackoverflow.com/questions/10073699/pad-a-number-with-leading-zeros-in-javascript
+// ("0000" + num).substr(-4,4); //short and sweet - 1st comment.
+// hours:   (('00' + (Math.floor(parameter/3600))).substr(-2,2))
+// minutes: (('00' + (Math.floor((parameter/60)%60))).substr(-2,2))
+// seconds: (('00' + (Math.floor(parameter%60))).substr(-2,2))
+
+// składnia str.substr(start[, length]) - Zwraca określoną liczbę początkowych znaków w łańcuchu znaków w określonej lokalizacji.
+// start - Lokalizacja, w której rozpoczyna się wyciąganie znaków (wartość liczbowa pomiędzy 0, a jeden znak mniej niż długość łańcucha znaków).
+// length - Liczba znaków do wyciągnięcia.
--- travel-agency/src/utils/utils.test.js
+++ travel-agency/src/utils/utils.test.js
@@ -1,1 +1,30 @@
 // testy funkcji formatTime.
+import { formatTime } from './formatTime';
+
+describe('utils', () => {
+  describe('formatTime', () => {
+  
+    it('should return null if there is no arg', () => { // Czy jeśli nie podano argumentu, to funkcja zwróci null?
+      expect(formatTime()).toBe(null);
+    });
+
+    it('should return null if arg is not a number', () => { // Czy jeśli podane coś innego niż liczbę, to funkcja zwróci null?
+      expect(formatTime('abc')).toBe(null);
+      expect(formatTime(() => {})).toBe(null);
+    });
+
+    it('should return null if arg is lower than zero', () => { // Czy jeśli podano liczbę mniejszą niż zero, to funkcja zwróci null?
+      expect(formatTime(-1)).toBe(null);
+      expect(formatTime(-2)).toBe(null);
+    });
+
+    it('should return time in hh:mm:ss if arg is proper', () => {
+      expect(formatTime(122)).toBe('00:02:02');
+      expect(formatTime(3793)).toBe('01:03:13');
+      expect(formatTime(120)).toBe('00:02:00');
+      expect(formatTime(3604)).toBe('01:00:04');
+    });
+  
+  });
+});
+
