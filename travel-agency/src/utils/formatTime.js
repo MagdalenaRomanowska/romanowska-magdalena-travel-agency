11ffc7b59be85120a9042b6882d12046aa96180f
Magdalena Romanowska <magdalena.romanowska.poczta@gmail.com>
2020-09-10 08:51:24 +0200

    Submodule 20.3 3rd change in formatTime.js

--- travel-agency/src/utils/formatTime.js
+++ travel-agency/src/utils/formatTime.js
@@ -1,13 +1,12 @@
-export const formatTime = parameter => {
-  if (parameter==undefined || isNaN(parameter) || parameter < 0){
+export const formatTime = parameter => { 
+  if (parameter==undefined || Number.isNaN(parameter) || parameter < 0){
     return null;
-  } else {
-    let hours = (('00' + Math.floor(parameter/3600)).substr(-2,2));
-    let minutes = (('00' + Math.floor((parameter/60)%60)).substr(-2,2));
-    let seconds = (('00' + Math.floor(parameter%60)).substr(-2,2));
-    let time = hours + ':' +  minutes + ':' + seconds;
-    return time;
-  }  
+  } 
+  let hours = (('00' + Math.floor(parameter/3600)).substr(-2,2));
+  let minutes = (('00' + Math.floor((parameter/60)%60)).substr(-2,2));
+  let seconds = (('00' + Math.floor(parameter%60)).substr(-2,2));
+  let time = hours + ':' +  minutes + ':' + seconds;
+  return time;
 };
 
 // zero padding: https://stackoverflow.com/questions/10073699/pad-a-number-with-leading-zeros-in-javascript
